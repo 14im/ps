be4fa3b081a4c93b4375668c91224bb2c643a4d6
Karim <karim.touahria@covea.fr>
2024-07-30 14:18:02 +0200

    last

--- ArchiveInstaller.ps1
+++ ArchiveInstaller.ps1
@@ -1,4 +1,3 @@
-[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PossibleIncorrectUsageOfRedirectionOperator','',Scope='Function')]
 class ArchiveInstaller {
     [string] $DownloadDirectory = $(Get-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders").PSObject.Properties["{374DE290-123F-4565-9164-39C4925E467B}"].Value
     [string] $DownloadUrl
@@ -71,7 +70,7 @@ class ArchiveInstaller {
     }
 
     [string] ExtractLastLocalArchive() {
-        return this.DestinationExtractionDirectory($this.GetLastLocalArchive())
+        return $this.DestinationExtractionDirectory($this.GetLastLocalArchive())
     }
     
     [string] DestinationExtractionDirectory() {
@@ -153,48 +152,6 @@ class GitArchiveInstaller : ArchiveInstaller {
 
 # stolen from https://stackoverflow.com/questions/69236623/adding-path-permanently-to-windows-using-powershell-doesnt-appear-to-work
 
-function Add-Path {
-    param(
-      [Parameter(Mandatory, Position=0)]
-      [string] $LiteralPath,
-      [ValidateSet('User', 'CurrentUser', 'Machine', 'LocalMachine')]
-      [string] $Scope 
-    )
-    Set-StrictMode -Version 1; $ErrorActionPreference = 'Stop'
-  
-    $isMachineLevel = $Scope -in 'Machine', 'LocalMachine'
-    
-    if ($isMachineLevel -and -not $($ErrorActionPreference = 'Continue'; net session 2>$null)) { throw "You must run AS ADMIN to update the machine-level Path environment variable." }  
-  
-    $regPath = 'registry::' + ('HKEY_CURRENT_USER\Environment', 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment')[$isMachineLevel]
-  
-    # Note the use of the .GetValue() method to ensure that the *unexpanded* value is returned.
-    $currDirs = (Get-Item -LiteralPath $regPath).GetValue('Path', '', 'DoNotExpandEnvironmentNames') -split ';' -ne ''
-  
-    if ($LiteralPath -in $currDirs) {
-      Write-Verbose "Already present in the persistent $(('user', 'machine')[$isMachineLevel])-level Path: $LiteralPath"
-      return
-    }
-  
-    $newValue = ($currDirs + $LiteralPath) -join ';'
-  
-    # Update the registry.
-    Set-ItemProperty -Type ExpandString -LiteralPath $regPath Path $newValue
-  
-    # Broadcast WM_SETTINGCHANGE to get the Windows shell to reload the
-    # updated environment, via a dummy [Environment]::SetEnvironmentVariable() operation.
-    $dummyName = [guid]::NewGuid().ToString()
-    [Environment]::SetEnvironmentVariable($dummyName, 'foo', 'User')
-    [Environment]::SetEnvironmentVariable($dummyName, [NullString]::value, 'User')
-  
-    # Finally, also update the current session's `$env:Path` definition.
-    # Note: For simplicity, we always append to the in-process *composite* value,
-    #        even though for a -Scope Machine update this isn't strictly the same.
-    $env:Path = ($env:Path -replace ';$') + ';' + $LiteralPath
-  
-    Write-Verbose "`"$LiteralPath`" successfully appended to the persistent $(('user', 'machine')[$isMachineLevel])-level Path and also the current-process value."
-  
-  }
 function Get-PowerShellArchive {
     param(
         $DownloadDirectory
@@ -353,3 +310,49 @@ function Install-Git {
     }
     $Destination
 }
+
+    #[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PossibleIncorrectUsageOfRedirectionOperator', '')]
+    function Add-Path {
+    param(
+      [Parameter(Mandatory, Position=0)]
+      [string] $LiteralPath,
+      [ValidateSet('User', 'CurrentUser', 'Machine', 'LocalMachine')]
+      [string] $Scope 
+    )
+    
+    Set-StrictMode -Version 1; 
+    $ErrorActionPreference = 'Stop'
+  
+    $isMachineLevel = $Scope -in 'Machine', 'LocalMachine'
+    
+    if ($isMachineLevel -and -not $($ErrorActionPreference = 'Continue'; net session 2>$null)) { throw "You must run AS ADMIN to update the machine-level Path environment variable." }  
+  
+    $regPath = 'registry::' + ('HKEY_CURRENT_USER\Environment', 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment')[$isMachineLevel]
+  
+    # Note the use of the .GetValue() method to ensure that the *unexpanded* value is returned.
+    $currDirs = (Get-Item -LiteralPath $regPath).GetValue('Path', '', 'DoNotExpandEnvironmentNames') -split ';' -ne ''
+  
+    if ($LiteralPath -in $currDirs) {
+      Write-Verbose "Already present in the persistent $(('user', 'machine')[$isMachineLevel])-level Path: $LiteralPath"
+      return
+    }
+  
+    $newValue = ($currDirs + $LiteralPath) -join ';'
+  
+    # Update the registry.
+    Set-ItemProperty -Type ExpandString -LiteralPath $regPath Path $newValue
+  
+    # Broadcast WM_SETTINGCHANGE to get the Windows shell to reload the
+    # updated environment, via a dummy [Environment]::SetEnvironmentVariable() operation.
+    $dummyName = [guid]::NewGuid().ToString()
+    [Environment]::SetEnvironmentVariable($dummyName, 'foo', 'User')
+    [Environment]::SetEnvironmentVariable($dummyName, [NullString]::value, 'User')
+  
+    # Finally, also update the current session's `$env:Path` definition.
+    # Note: For simplicity, we always append to the in-process *composite* value,
+    #        even though for a -Scope Machine update this isn't strictly the same.
+    $env:Path = ($env:Path -replace ';$') + ';' + $LiteralPath
+  
+    Write-Verbose "`"$LiteralPath`" successfully appended to the persistent $(('user', 'machine')[$isMachineLevel])-level Path and also the current-process value."
+  
+  }
